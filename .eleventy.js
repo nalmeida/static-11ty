1ecea7aefbe98f2ecf9b3dbd965922d2edf0e7fe
Nicholas <nicholas.almeida@gympass.com>
2022-11-10 08:57:42 -0300

    breacking change: changed all the templates to njk

--- .eleventy.js
+++ .eleventy.js
@@ -1,29 +1,36 @@
-module.exports = function(eleventyConfig) {
+var site = require('./src/_data/site');
 
-	function removeTrailingSlash(str) {
-		return str.replace(/\/+$/, '');
-	}
+module.exports = function(eleventyConfig) {
 
 	eleventyConfig.addPassthroughCopy('src/assets/fonts');
 	eleventyConfig.addPassthroughCopy('src/assets/img');
 	eleventyConfig.addPassthroughCopy('src/favicon.ico');
 	eleventyConfig.addPassthroughCopy('src/robots.txt');
 
-	const UUID = Math.random().toString(36).slice(-8);
+	// Filters
+	eleventyConfig.addFilter('dateToISO', (dateObj) => {
+		let _date = new Date(dateObj);
+    return _date.toISOString().split('T')[0];
+  });
 
+	// Shortcodes
+	const UUID = Math.random().toString(36).slice(-8);
 	eleventyConfig.addShortcode('VERSION', function (value) {
 		return `${UUID}`;
 	});
 
-	const BASE_URL = removeTrailingSlash(process.env.BASE_URL || 'http://localhost:8080');
-	
+	function removeTrailingSlash(str) {
+		return str.replace(/\/+$/, '');
+	}
+	const BASE_URL = removeTrailingSlash(process.env.BASE_URL || site.BASE_URL);
 	eleventyConfig.addShortcode('BASE_URL', function (value) {
 		return BASE_URL;
 	});
 
 	return {
-		templateFormats: ['html', 'liquid'],
+		templateFormats: ['html', 'njk', 'liquid'],
 		dir: {
+			data: '_data',
 			input: 'src',
 			output: '_site'
 		}
